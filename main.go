4b7d44cb9090a5b479f2702564e1f1f81bb0131e
AlexKulam <alexkulam45@gmail.com>
2024-10-15 13:43:54 +0300

    реализация вопрос-ответ

--- main.go
+++ main.go
@@ -7,6 +7,14 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
+type UserTest struct {
+	ChatID  int64
+	Current int
+	Answers []string
+}
+
+var tests = make(map[int64]*UserTest)
+
 func main() {
 	token, err := os.ReadFile("token.txt")
 	if err != nil {
@@ -29,7 +37,6 @@ func main() {
 		if update.Message != nil {
 			switch update.Message.Text {
 			case "/start":
-
 				keyboard := tgbotapi.NewInlineKeyboardMarkup(
 					tgbotapi.NewInlineKeyboardRow(
 						tgbotapi.NewInlineKeyboardButtonData("Войти", "login"),
@@ -37,13 +44,16 @@ func main() {
 				)
 
 				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Добро пожаловать на тест! Войдите чтобы начать тест")
-
 				msg.ReplyMarkup = keyboard
 				bot.Send(msg)
 
 			default:
-				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Я не понимаю. Используйте /help для получения информации.")
-				bot.Send(msg)
+				if test, exists := tests[update.Message.Chat.ID]; exists {
+					handleAnswer(test, update.Message.Text, bot)
+				} else {
+					msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Я не понимаю. Используйте /help для получения информации.")
+					bot.Send(msg)
+				}
 			}
 		} else if update.CallbackQuery != nil {
 			switch update.CallbackQuery.Data {
@@ -55,8 +65,56 @@ func main() {
 }
 
 func handleLogin(chatID int64, bot *tgbotapi.BotAPI) {
-	msg := tgbotapi.NewMessage(chatID, "Вы нажали 'Войти'. Здесь будет логика входа.")
-	bot.Send(msg)
+	test := &UserTest{
+		ChatID:  chatID,
+		Current: 0,
+		Answers: make([]string, 0),
+	}
+	tests[chatID] = test
+	startTest(test, bot)
 }
 
-//dima loh
+func startTest(test *UserTest, bot *tgbotapi.BotAPI) {
+	questions := []string{
+		"Вопрос 1:",
+		"Вопрос 2:",
+		"Вопрос 3:",
+		"Вопрос 4:",
+		"Вопрос 5:",
+		"Вопрос 6:",
+		"Вопрос 7:",
+		"Вопрос 8:",
+		"Вопрос 9:",
+		"Вопрос 10:",
+	}
+
+
+
+
+	if test.Current < len(questions) {
+		msg := tgbotapi.NewMessage(test.ChatID, questions[test.Current])
+		bot.Send(msg)
+	} else {
+		finishTest(test, bot)
+	}
+
+	
+}
+
+func handleAnswer(test *UserTest, answer string, bot *tgbotapi.BotAPI) {
+
+	
+	test.Answers = append(test.Answers, answer)
+	test.Current++
+	startTest(test, bot)
+}
+
+func finishTest(test *UserTest, bot *tgbotapi.BotAPI) {
+	msg := tgbotapi.NewMessage(test.ChatID, "Тест успешно пройден! Возвращайтесь на сайт: [ссылка](https://example.com)")
+	msg.ParseMode = "Markdown"
+	bot.Send(msg)
+
+	log.Printf("Ответы пользователя %d: %v", test.ChatID, test.Answers)
+
+	delete(tests, test.ChatID)
+}
