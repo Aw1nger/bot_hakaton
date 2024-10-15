4d7ed646d01888fd5f484470d3a284afe8cb7238
AlexKulam <alexkulam45@gmail.com>
2024-10-15 12:17:12 +0300

    подготовка к реализации входа

--- main.go
+++ main.go
@@ -1,34 +1,60 @@
 package main
 
 import (
-    "log"
-    "os"
-    "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"log"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 func main() {
-    token, err := os.ReadFile("token.txt")
-    if err != nil {
-        log.Fatalf("Ошибка чтения файла с токеном: %v", err)
-    }
+	token, err := os.ReadFile("token.txt")
+	if err != nil {
+		log.Fatalf("Ошибка чтения файла с токеном: %v", err)
+	}
 
-    bot, err := tgbotapi.NewBotAPI(string(token))
-    if err != nil {
-        log.Fatalf("Ошибка создания бота: %v", err)
-    }
+	bot, err := tgbotapi.NewBotAPI(string(token))
+	if err != nil {
+		log.Fatalf("Ошибка создания бота: %v", err)
+	}
 
-    log.Printf("Авторизован как %s", bot.Self.UserName)
+	log.Printf("Authorized on account %s", bot.Self.UserName)
 
-    u := tgbotapi.NewUpdate(0)
-    u.Timeout = 60
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
 
-    updates := bot.GetUpdatesChan(u)
+	updates := bot.GetUpdatesChan(u)
 
-    for update := range updates {
-        if update.Message != nil {
-            log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-            msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Привет! Ты написал: "+update.Message.Text)
-            bot.Send(msg)
-        }
-    }
+	for update := range updates {
+		if update.Message != nil {
+			switch update.Message.Text {
+			case "/start":
+
+				keyboard := tgbotapi.NewInlineKeyboardMarkup(
+					tgbotapi.NewInlineKeyboardRow(
+						tgbotapi.NewInlineKeyboardButtonData("Войти", "login"),
+					),
+				)
+
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Добро пожаловать на тест! Войдите чтобы начать тест")
+
+				msg.ReplyMarkup = keyboard
+				bot.Send(msg)
+
+			default:
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Я не понимаю. Используйте /help для получения информации.")
+				bot.Send(msg)
+			}
+		} else if update.CallbackQuery != nil {
+			switch update.CallbackQuery.Data {
+			case "login":
+				handleLogin(update.CallbackQuery.Message.Chat.ID, bot)
+			}
+		}
+	}
+}
+
+func handleLogin(chatID int64, bot *tgbotapi.BotAPI) {
+	msg := tgbotapi.NewMessage(chatID, "Вы нажали 'Войти'. Здесь будет логика входа.")
+	bot.Send(msg)
 }
